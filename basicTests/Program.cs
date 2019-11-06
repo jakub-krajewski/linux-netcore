bd7f8bf6f87ab354123679904f75f84188928f3e
Jakub Krajewski <jkbkrjwsk@wp.pl>
2019-11-06 22:23:02 +0100

    initial tests

---
+++ basicTests/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+namespace basicTests
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            Console.WriteLine("Wait for it... for a few seconds!");
+            await Task.Delay(3000);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
