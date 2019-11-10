c7ad0ce3f05a62b454352b1fc6be792a72451a30
Jakub Krajewski <jkbkrjwsk@wp.pl>
2019-11-10 23:19:14 +0100

    just playing

--- basicTests/Program.cs
+++ basicTests/Program.cs
@@ -8,6 +8,10 @@ namespace basicTests
         {
             Console.WriteLine("Wait for it... for a few seconds!");
             await Task.Delay(3000);
+            Console.Clear();
+            Console.WriteLine("Just one more second...");
+            await Task.Delay(1000);
+            Console.Clear();
             Console.WriteLine("Hello World!");
         }
     }
